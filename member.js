195f9f27db8e46ef027a5ab02481dfc21de0d561
Leonardo Brito Theodoro <leonardo.theodoro@linx.com.br>
2025-04-22 16:36:36 -0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+    return {
+        name: "Member",
+        description: "Member of the team",
+        skills: [
+            {
+                name: "JavaScript",
+                level: 3,
+            },
+            {
+                name: "Python",
+                level: 2,
+            },
+            {
+                name: "Java",
+                level: 1,
+            },
+        ],
+        };
+    }

d04b1c1333c0dcf2f0830485a7251b41831d3897
Leonardo Brito Theodoro <leonardo.theodoro@linx.com.br>
2025-04-22 16:48:19 -0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+const commentsFilePath = path.join(__dirname, 'comments.json');
+const comments = [];
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
+// Middleware
